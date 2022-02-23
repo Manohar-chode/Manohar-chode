8371e22aee8918835988cac2c180b2079379e379
Manohar-chode <chode.manohar@gmail.com>
2022-02-23 22:43:44 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import Expenses from './components/Expenses';
+function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's Get Started!</h2>
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+export default App;
