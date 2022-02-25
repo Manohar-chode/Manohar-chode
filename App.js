fa9f283523999fb4aeef6ced9d85f962b50fb4aa
Manohar-chode <chode.manohar@gmail.com>
2022-02-25 13:03:37 +0530

    state

---
+++ App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class App extends Component() {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
+    
